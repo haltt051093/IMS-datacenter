81bd42842dcd88cefee4adb89a0dcebc7aac0a06
unknown <mendozalz>
2016-02-21 15:05:43 +0000

    fix datetimepicker

---
+++ CODE/IMS/Models/HomeSidebarViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace IMS.Models
+{
+    public class HomeSidebarViewModel
+    {
+        public HomeSidebarViewModel()
+        {
+            SubMenu = new List<HomeSidebarViewModel>();
+        }
+
+        public HomeSidebarViewModel(string title, string action, string controller)
+        {
+            SubMenu = new List<HomeSidebarViewModel>();
+        }
+
+        public string Title { get; set; }
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public List<string> Roles { get; set; }
+        public List<HomeSidebarViewModel> SubMenu { get; set; }
+    }
+}
