b82a3bf1f35bd1cd5ba6e76c4da7594ecdd79005
NamCao <namchse61061@fpt.edu.vn>
2016-02-22 16:08:36 +0700

    check conflict

--- CODE/IMS.Data/Repository/RackDAO.cs
+++ CODE/IMS.Data/Repository/RackDAO.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using IMS.Data.Generic;
 using IMS.Data.Models;
 
--- CODE/IMS/Models/RequestRentRackViewModel.cs
+++ CODE/IMS/Models/RequestRentRackViewModel.cs
@@ -1,12 +1,22 @@
-﻿using IMS.Data.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using IMS.Data.Models;
 
 namespace IMS.Models
 {
     public class RequestRentRackViewModel
     {
         public int RackNumbers { get; set; }
+
         public string Customer { get; set; }
+        public string RequestCode { get; set; }
+        public string RequestType { get; set; }
         public string Description { get; set; }
-        public Request RequestRentRack { get; set; }
+        public DateTime? RequestedTime { get; set; }
+        public string StatusCode { get; set; }
+        public bool IsViewed { get; set; }
+        public List<SelectListItem> AvailableRacks { get; set; }
+        public List<Rack> SelectedRacks { get; set; } 
     }
 }
