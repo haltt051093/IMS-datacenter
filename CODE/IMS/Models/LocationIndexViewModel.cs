6fc06f47d5138a78f2443669c2a0a01e9576665f
tienhlhse60932 <tienhlhse60932@fpt.edu.vn>
2016-03-01 09:42:33 +0700

    validate rack name

--- CODE/IMS/Models/LocationIndexViewModel.cs
+++ CODE/IMS/Models/LocationIndexViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using IMS.Data.Models;
 using IMS.Data.ViewModels;
 
@@ -16,6 +17,9 @@ namespace IMS.Models
         public string Request { get; set; }
         public Server Server { get; set; }
         public string LocationCode { get; set; }
+
+        [Required(ErrorMessage = "Please input Rack Name!")]
+        [RegularExpression("^[A-Z][1-9]$", ErrorMessage = "Wrong format of Rack Name! Try again!")]
         public string RackName { get; set; }
         public int? MaximumPower { get; set; }
     }
