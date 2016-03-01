81c4363171763eaa0adaf59aff0c72a86ca1d698
tienhlhse60932 <tienhlhse60932@fpt.edu.vn>
2016-03-01 11:01:08 +0700

    Validate network address

--- CODE/IMS/Models/IPIndexViewModel.cs
+++ CODE/IMS/Models/IPIndexViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 using IMS.Data.ViewModels;
 
@@ -16,6 +17,12 @@ namespace IMS.Models
         public string NewIP { get; set; }
         public string ServerCode { get; set; }
         public List<SelectListItem> ListNewIP { get; set; }
+
+        [Required(ErrorMessage = "Please input Network Address!")]
+        [RegularExpression("^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+        "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+        "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+        "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$", ErrorMessage = "Wrong format of Network Address! Try again!")]
         public string Address { get; set; }
         public int Netmask { get; set; }
     }
