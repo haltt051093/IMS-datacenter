7e0aa8aba384d5a6be66d6bcc85496cc6c0ad41d
tienhlhse60932 <tienhlhse60932@fpt.edu.vn>
2016-03-05 13:52:13 +0700

    Update code Location/Index2

--- CODE/IMS/ApiControllers/RackController.cs
+++ CODE/IMS/ApiControllers/RackController.cs
@@ -11,26 +11,46 @@ namespace IMS.ApiControllers
     {
         public List<string> GetAvailableName(string term)
         {
-            var allRacks = RackBLO.Current.GetAll().Select(x => x.RackName).ToList();
-            var filteredRacks = allRacks
-                .Where(x => x.StartsWith(term, StringComparison.InvariantCultureIgnoreCase))
-                .Select(x => x.Length > 1 ?  x.Substring(1) : "0")
-                .ToList();
-            var rackNumbers = filteredRacks
-                .Select(x => x.ToInt(0))
-                .OrderBy(x => x)
-                .ToList();
-            var result = new List<string>();
-            int count = 1;
-            while (result.Count < 10)
+            if (term.Length == 1)
             {
-                if (!rackNumbers.Contains(count))
+                var allRacks = RackBLO.Current.GetAll().Select(x => x.RackName).ToList();
+                var filteredRacks = allRacks
+                    .Where(x => x.StartsWith(term, StringComparison.InvariantCultureIgnoreCase))
+                    .Select(x => x.Length > 1 ? x.Substring(1) : "0")
+                    .ToList();
+                var rackNumbers = filteredRacks
+                    .Select(x => x.ToInt(0))
+                    .OrderBy(x => x)
+                    .ToList();
+                var result = new List<string>();
+                int count = 1;
+                while (result.Count < 10)
                 {
-                    result.Add(term.ToUpper() + count);
+                    if (!rackNumbers.Contains(count))
+                    {
+                        result.Add(term.ToUpper() + count);
+                    }
+                    count++;
                 }
-                count++;
+                return result;
             }
-            return result;
+            else return null;
+
+        }
+
+        public string CheckDuplicate(string rack)
+        {
+            var racks = RackBLO.Current.GetAll().Select(x => x.RackName).ToList();
+            int count = 0;
+            for (int i = 0; i < racks.Count; i++)
+            {
+                if (racks[i] == rack) count++;
+            }
+            if (count > 0)
+            {
+                return "The Rack Name was existed!";
+            }
+            else return null;
         }
     }
 }
