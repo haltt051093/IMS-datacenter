d6988e310e8c648d4433c20c4cda0e56f66302c7
tienhlhse60932 <tienhlhse60932@fpt.edu.vn>
2016-03-22 15:40:15 +0700

    Update SuccessMessage

--- CODE/IMS/Models/AccountLoginViewModel.cs
+++ CODE/IMS/Models/AccountLoginViewModel.cs
@@ -8,6 +8,7 @@ namespace IMS.Models
         public string UserName { get; set; }
         public string Password { get; set; }
         public string ReturnUrl { get; set; }
+        public string SuccessMessage { get; set; }
         [Required]   
         [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
