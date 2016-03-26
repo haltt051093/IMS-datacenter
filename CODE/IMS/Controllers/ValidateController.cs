72392329efd631609fbbe90d1b447a9d02fcba31
tienhlhse60932 <tienhlhse60932@fpt.edu.vn>
2016-03-26 09:54:04 +0700

    Update validate

--- CODE/IMS/Controllers/ValidateController.cs
+++ CODE/IMS/Controllers/ValidateController.cs
@@ -1,10 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using IMS.Data.Business;
 using IMS.Data.Models;
+using IMS.Data.Repository;
 
 namespace IMS.Controllers
 {
-    public class ValidateController : Controller
+    public class ValidateController : CoreController
     {
         [HttpPost]
         public JsonResult CheckRackName(string RackName)
@@ -25,6 +27,21 @@ namespace IMS.Controllers
         {
             var existing = AccountBLO.Current.GetByKeys(new Account {Username = username});
             return Json(existing == null);
+        }
+
+        [HttpPost]
+        public JsonResult CheckEmail(string Email)
+        {
+            var existing = AccountDAO.Current.Query(x => x.Email == Email).FirstOrDefault();
+            return Json(existing != null);
+        }
+
+        [HttpPost]
+        public JsonResult CheckPassword(string Password)
+        {
+            var user = GetCurrentUserName();
+            var existing = AccountDAO.Current.Query(x => x.Username == user && x.Password == Password).FirstOrDefault();
+            return Json(existing != null);
         }
     }
 }
--- CODE/IMS/Models/AccountLoginViewModel.cs
+++ CODE/IMS/Models/AccountLoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace IMS.Models
 {
@@ -12,6 +13,7 @@ namespace IMS.Models
         public string FailMessage { get; set; }
         [Required]   
         [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Email is not valid")]
+        [Remote("CheckEmail","Validate",ErrorMessage = "Email is not existed", HttpMethod = "POST")]
         public string Email { get; set; }
     }
 }
